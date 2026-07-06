a1ed4212014715699f3f848646f06e4df823aa12
giangtong2010 <giangtong31122010@gmail.com>
2026-07-06 13:02:37 +0700

    Stop tracking build directory

--- build/aten/Aten/CMakeFiles/Aten.dir/src/_reduction_ops.cpp.obj.ddi.d
+++
@@ -1,2 +0,0 @@
-aten\Aten\CMakeFiles\Aten.dir\src\_reduction_ops.cpp.obj.ddi: \
-  D:\TensorForge\aten\Aten\src\_reduction_ops.cpp
--- build/aten/Aten/CMakeFiles/Aten.dir/src/_view_ops.cpp.obj.ddi.d
+++
@@ -1,2 +0,0 @@
-aten\Aten\CMakeFiles\Aten.dir\src\_view_ops.cpp.obj.ddi: \
-  D:\TensorForge\aten\Aten\src\_view_ops.cpp
